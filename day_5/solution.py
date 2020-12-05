1274a40173873e2f37ee981acc39b6c1b4d379ac
Alberto Polidura Perez <albertopp@unlimiteck.com>
2020-12-05 16:48:42 +0100

    Day 5 solutions

---
+++ day_5/solution.py
@@ -0,0 +1,25 @@
+import math
+
+
+def binary_space_partition(line, size, fn):
+    left = 0
+    right = size
+    for char in line:
+        if char == 'F' or char == 'L':
+            right = right - math.ceil((right-left)/2)
+            res = left
+        elif char == 'B' or char == 'R':
+            left = left + math.ceil((right-left)/2)
+            res = right
+    return res
+
+seat_ids = []
+with open("input.txt", 'r') as file:
+    for line in file:
+        row = binary_space_partition(line[:7], 127, min)
+        col = binary_space_partition(line[7:], 7, max)
+        seat_id = row*8 + col
+        seat_ids.append(seat_id)
+
+print(set(range(min(seat_ids), max((seat_ids))))-set(seat_ids))
+print(max(seat_ids))
